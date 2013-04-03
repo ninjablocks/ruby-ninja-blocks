6c56bd57d5a93f03ef926d99edd1062cee150ac8
Marcus Schappi <marcus@schappi.com>
2013-04-03 15:54:44 +1100

    stub out more rules methods

--- lib/ninja_blocks/rule.rb
+++ lib/ninja_blocks/rule.rb
@@ -5,6 +5,31 @@ module NinjaBlocks
       get('https://api.ninja.is/rest/v0/rule')
     end
 
+
+    def create
+      # create rule
+    end
+    
+    def get
+      # get rule
+    end
+    
+    def update
+      # update a rule
+    end
+    
+    def delete
+      # delete a rule
+    end
+    
+    def suspend
+      # suspend a rule
+    end
+    
+    def unsuspend
+      # unsuspend a rule
+    end
+
   end
 end
 
